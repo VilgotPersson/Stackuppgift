ddaf0b66ae65f437d03c59fbd0b84693d00f1dee
VilgotPersson <69978046+VilgotPersson@users.noreply.github.com>
2022-03-14 14:06:07 +0100

    Add files via upload

---
+++ Stack/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> stack = new Stack<char>();
+            string str = "(){[}[]";
+
+            int i = 0;
+
+            foreach (char k in str)
+                stack.Push(k);
+
+            while(stack.Count > 0)
+            {
+                char k = stack.Pop();
+                if (k == '(')
+                {
+                    i++;
+                }
+                else if (k == ')')
+                {
+                    i--;
+                }
+                else if (k == '{')
+                {
+                    i++;
+                }
+                else if (k == '}')
+                {
+                    i--;
+                }
+                else if (k == '[')
+                {
+                    i++;
+                }
+                else if (k == ']')
+                {
+                    i--;
+                }
+            }
+            if (i == 0)
+            {
+                Console.WriteLine("Balanserad");
+            }
+            else
+            {
+                Console.WriteLine("Inte balanserad");
+            }
+        }
+    }
+}
